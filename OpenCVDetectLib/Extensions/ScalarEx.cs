97221b8fc9cabd8817cc3244de868bf8b7558007
hyrw <xiehui88i888@gmail.com>
2025-04-08 15:08:16 +0800

    using

--- OpenCVDetectLib/Extensions/ScalarEx.cs
+++ OpenCVDetectLib/Extensions/ScalarEx.cs
@@ -4,13 +4,10 @@ namespace ShortCircuitDetect.Lib.Extensions;
 
 public static class ScalarEx
 {
-    public static (Scalar lower, Scalar upper) GetHRange(this Scalar self, int threshold = 20)
+    public static (Scalar lower, Scalar upper) GetHRange(this Scalar bgr, int threshold = 20)
     {
-        //var lower = new Scalar(self[0] - threshold, self[1] - threshold, self[2] - threshold);
-        //var upper = new Scalar(self[0] + threshold, self[1] + threshold, self[2] + threshold);
-
-        var mat = new Mat(new Size(1, 1), MatType.CV_8UC3, self);
-        var color = mat.CvtColor(ColorConversionCodes.BGR2HSV);
+        using Mat mat = new Mat(new Size(1, 1), MatType.CV_8UC3, bgr);
+        using Mat color = mat.CvtColor(ColorConversionCodes.BGR2HSV);
         var hsv = color.At<Vec3b>();
         var lower = new Scalar(hsv[0] - threshold, 0, 0);
         var upper = new Scalar(hsv[1] + threshold, 255, 255);
