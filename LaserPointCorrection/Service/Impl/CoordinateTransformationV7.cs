9da0f777f4b076e4e7516a34a0defc524fc84459
hyrw <xiehui88i888@gmail.com>
2025-04-14 16:39:22 +0800

    振镜坐标校正V7

---
+++ LaserPointCorrection/Service/Impl/CoordinateTransformationV7.cs
@@ -0,0 +1,73 @@
+using OpenCvSharp;
+
+///<summary>
+///分段线性插值
+///</summary>
+public class CoordinateTransformationV7 : ICoordinateTransformation
+{
+
+    readonly BilinearInterpolator xInterpolator;
+    readonly BilinearInterpolator yInterpolator;
+
+    public CoordinateTransformationV7(IList<Point> theory, IList<Point> real)
+    {
+        if (theory is null || real is null) throw new ArgumentNullException($"theory or real is null");
+
+        if (theory.Count == 9)
+        {
+            (var x, var y, var z_x, var z_y) = MakeData(theory, real, 3);
+            this.xInterpolator = new BilinearInterpolator(x, y, z_x);
+            this.yInterpolator = new BilinearInterpolator(x, y, z_y);
+        }
+        else if (theory.Count == 25)
+        {
+            (var x, var y, var z_x, var z_y) = MakeData(theory, real, 5);
+            this.xInterpolator = new BilinearInterpolator(x, y, z_x);
+            this.yInterpolator = new BilinearInterpolator(x, y, z_y);
+        }
+        else
+        {
+            throw new ArgumentException($"只支持9/25点");
+        }
+    }
+
+    public IList<Point> CorrectionCoordinate(IList<Point> path) => GetPath(path);
+
+    List<Point> GetPath(IList<Point> path)
+    {
+        if (path == null || path.Count == 0) throw new ArgumentException("路径不能为空");
+        List<Point> result = new(path.Count);
+        for (int i = 0; i < path.Count; i++)
+        {
+            var point = path[i];
+            var x = xInterpolator.Interpolate(point.X, point.Y);
+            var y = yInterpolator.Interpolate(point.X, point.Y);
+            var diffX = point.X - x;
+            var diffY = point.Y - y;
+
+            result.Add(new Point(x + diffX, y + diffY));
+        }
+        return result;
+    }
+
+    (double[] x, double[] y, double[,] xValue, double[,] yValue) MakeData(IList<Point> src, IList<Point> dst, int n)
+    {
+        double[] x = [.. src.Select(p => (double)p.X).Distinct().Order().Take(n)];
+        double[] y = [.. src.Select(p => (double)p.Y).Distinct().Order().Take(n)];
+        double[,] z_x = new double[n, n];
+        double[,] z_y = new double[n, n];
+
+        for (int i = 0; i < n; i++)
+        {
+            List<Point> row = row = dst.Skip(i * n).Take(n).ToList();
+            for (int j = 0; j < n; j++)
+            {
+                z_x[i, j] = row[j].X;
+                z_y[i, j] = row[j].Y;
+            }
+        }
+
+        return (x, y, z_x, z_y);
+    }
+
+}
