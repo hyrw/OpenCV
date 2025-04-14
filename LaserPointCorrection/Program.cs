df147c4ae5ca587c1b1d806059e88db6a1d8f004
hyrw <xiehui88i888@gmail.com>
2025-04-14 16:40:17 +0800

    demo

--- LaserPointCorrection/Program.cs
+++ LaserPointCorrection/Program.cs
@@ -1,4 +1,5 @@
 ﻿using OpenCvSharp;
+using static ICoordinateTransformationFactory;
 
 // 基于像素坐标点范围
 var laserXRange = (50, 200);
@@ -15,7 +16,7 @@ Point[] samplePoint = [
 ];
 CoordinateCorrection coordinateCorrection = new CoordinateCorrection(laserXRange, laserYRange, 25);
 coordinateCorrection.Correction();
-IList<Point> result = coordinateCorrection.CorrectionCoordinate(samplePoint);
+IList<Point> result = coordinateCorrection.CorrectionCoordinate(srcPoint);
 
 using Mat beforImg = Mat.Zeros(900, 800, MatType.CV_8UC3);
 using Mat afterImg = Mat.Zeros(900, 800, MatType.CV_8UC3);
@@ -24,7 +25,7 @@ for (int i = 0; i < srcPoint.Count; i++)
 {
     Point p = srcPoint[i];
     Point sp = samplePoint[i];
-    Cv2.Circle(beforImg, p, 2, Scalar.Green, -1);
+    Cv2.Circle(beforImg, p, 5, Scalar.Green, -1);
     Cv2.DrawMarker(beforImg, sp, Scalar.Red, MarkerTypes.Cross, 10);
 }
 
@@ -32,7 +33,7 @@ for (int i = 0; i < srcPoint.Count; i++)
 {
     Point p = srcPoint[i];
     Point r = result[i];
-    Cv2.Circle(afterImg, p, 2, Scalar.Green, -1);
+    Cv2.Circle(afterImg, p, 5, Scalar.Green, -1);
     Cv2.DrawMarker(afterImg, r, Scalar.Red, MarkerTypes.Cross, 10);
 }
 
@@ -83,8 +84,18 @@ public class CoordinateCorrection
     /// </summary>
     IList<Point> FindIntersectionPoints(Mat img)
     {
-        IIntersectionDetector intersectionDetector = new SkeletonizeCrossPoint();
-        return intersectionDetector.FindCrossPoints(img);
+        //IIntersectionDetector intersectionDetector = new SkeletonizeCrossPoint();
+        //return intersectionDetector.FindCrossPoints(img);
+        // 模拟的假数据
+        Point[] samplePoint = [
+            new(52, 48), new(88, 48), new (125, 48), new (161, 48), new(198, 48),
+            new(52, 85), new(88, 85), new (125, 85), new (161, 85), new(198, 85),
+            new(52, 123), new(88, 123), new (125, 123), new (161, 123), new(198, 123),
+            new(52, 160), new(88, 160), new (125, 160), new (161, 160), new(198, 160),
+            //new(52, 198), new(88, 198), new (125, 198), new (162, 198), new(198,198),
+            new(52, 202), new(88, 202), new (125, 202), new (162, 202), new(198,202),
+        ];
+        return samplePoint;
     }
 
     /// <summary>
@@ -95,15 +106,11 @@ public class CoordinateCorrection
         IList<Point> srcPoint = LaserMarking(this.laserXRange, this.laserXRange, this.numPoints);
         using Mat gray = AcquireCameraImage();
         IList<Point> crossPoints = FindIntersectionPoints(gray);
+
         if (crossPoints is null || crossPoints.Count != srcPoint.Count) throw new Exception("理论点和标刻点数量不一致");
         int width = this.laserXRange.Max - this.laserXRange.Min;
         int height = this.laserYRange.Max - this.laserYRange.Min;
-        this.coordinateTransformation = ICoordinateTransformationFactory.Create(
-                crossPoints.Take(srcPoint.Count).ToList(),
-                srcPoint,
-                ICoordinateTransformationFactory.CoordinateTransformationImpl.V6,
-                width,
-                height);
+        this.coordinateTransformation = ICoordinateTransformationFactory.Create(srcPoint, crossPoints, CoordinateTransformationImpl.V7, width, height);
     }
 
     /// <summary>
