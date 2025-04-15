77db13e1284278d6e50c70e132e6b8af12bdb100
hyrw <xiehui88i888@gmail.com>
2025-04-15 16:29:51 +0800

    增加参数

--- OpenCVDetectLib/Service/DefectOptions.cs
+++ OpenCVDetectLib/Service/DefectOptions.cs
@@ -4,13 +4,6 @@ namespace ShortCircuitDetect.Lib.Service;
 
 public class DefectOptions
 {
-    public int RMin { get; set; }
-    public int RMax { get; set; }
-    public int GMin { get; set; }
-    public int GMax { get; set; }
-    public int BMin { get; set; }
-    public int BMax { get; set; }
-
     public int HMin { get; set; }
     public int HMax { get; set; }
     public int SMin { get; set; }
@@ -18,17 +11,33 @@ public class DefectOptions
     public int VMin { get; set; }
     public int VMax { get; set; }
 
+    public int RMin { get; set; }
+    public int RMax { get; set; }
+    public int GMin { get; set; }
+    public int GMax { get; set; }
+    public int BMin { get; set; }
+    public int BMax { get; set; }
 
     public int MinArea { get; set; }
     public int Thresh { get; set; }
 
     public int OpenWidth { get; set; }
     public int OpenHeight { get; set; }
+    public int OpenIterations { get; set; }
 
     public int CloseWidth { get; set; }
     public int CloseHeight { get; set; }
+    public int CloseIterations { get; set; }
 
+    public int ErodeWidth { get; set; }
+    public int ErodeHeight { get; set; }
+    public int ErodeIterations { get; set; }
+
+    public int DilateWidth { get; set; }
+    public int DilateHeight { get; set; }
+    public int DilateIterations { get; set; }
+
+    public TemplateMatchModes MatchMode;
     public double MatchLocScore { get; set; }
-    public TemplateMatchModes MatchMode { get; set; }
 
 }
